a145debc4d7266f1edec7ff2a5b5007ebc07bfe4
Philip Helsted <philiphelsted@me.com>
2023-04-18 09:12:18 +0200

    Initial scrape functional

---
+++ util/wiki_util.py
@@ -0,0 +1,16 @@
+
+def hyperlink_cleanup(str_link):
+    link_prefix = "https://en.wikipedia.org"
+    if "/wiki/" in str_link:
+        out = link_prefix + str_link
+        if '#' in out:
+            out = out.split('#')[0]
+        if 'Wikipedia' in out:
+            return False
+        if 'Template' in out:
+            return False
+        return out
+    else:
+        return False
+
+
